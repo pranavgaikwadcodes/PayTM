c299aa2c7d3d93fd2064da35c987dbabc4575d16
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-10-29 12:58:33 +0530

    Implimented Transactions

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,9 +1,59 @@
 const express = require('express')
 const router = express.Router()
 const zod = require('zod')
+const { authMiddleware } = require('../middleware')
+const { Account } = require('../db')
+const { default: mongoose } = require('mongoose')
 
-router.get('', (req, res) => {
-    
+const transferMoneySchmea = zod.object({
+    to: zod.string(),
+    amount: zod.number().positive(),
+})
+
+router.get('/balance', authMiddleware, async (req, res) => {
+    const userDetails = await Account.findOne({ userId: req.userID})
+    res.status(200).json({
+        balance: userDetails.balance
+    })
+})
+
+router.post('/transfer', authMiddleware, async (req, res)=> {
+    const {success} = transferMoneySchmea.safeParse(req.body)
+    if (!success) {
+        return res.status(400).json({error: 'Invalid request body'})
+    }
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const { amount, to } = req.body;
+    const sender = req.userID
+
+    // fetching sender account
+    const senderAccount = await Account.findOne({ userId: sender }).session(session);
+
+    // checking acc balance of sender
+    if(senderAccount.balance < req.body.amount || !senderAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({error: 'Insufficient balance'})
+    }
+
+    // fetching receiver account
+    const receiverAccount = await Account.findOne({ userId: to }).session(session);
+    if(!receiverAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({error: 'Receiver account not found'})
+    }
+
+    // perform the transactions
+    await Account.updateOne( { userId: sender }, { $inc: {balance: -amount} } ).session(session)
+    await Account.updateOne( { userId: to }, { $inc: {balance: amount} } ).session(session)
+
+    // commit the Transaction
+    await session.commitTransaction();
+
+    res.status(200).json({
+        message: 'Transfer Successful'
+    })
 })
 
 module.exports = router
